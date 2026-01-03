902f91d33a46956d98a260ab13382522f666c266
Somnath Mishra <somnathmishra100dbi@gmail.com>
2026-01-03 09:33:46 +0530

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 import { requireAuth } from "@/modules/auth/utils/auth-utils";
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   await requireAuth();
   
-  return (
-    <div>
-      CodeRabbit Yooo!
-    </div>
-  );
+  redirect("/dashboard");
 }
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useSession } from "@/lib/auth-client";
+import { useEffect, useState } from "react"
+import { Sidebar, SidebarContent, SidebarHeader } from "./ui/sidebar";
+
+export const AppSidebar = () => {
+  const [mounted, setMounted] = useState(false);
+
+  const getSession = useSession;
+  const {data: session} = getSession.get()
+
+  useEffect(() => {
+    setMounted(true);
+  },[])
+
+  if (!mounted || !session) {
+    return null;
+  }
+
+  const user = session.user;
+  const name = user.name || "Guest";
+  const email = user.email
+  const userInitials = name.split(" ").map(n => n[0]).join("").toUpperCase()
+  
+  
+  return (
+    <Sidebar>
+      <SidebarHeader className="border-b">
+
+      </SidebarHeader>
+
+      <SidebarContent className="px-3 py-6 flex-col gap-1">
+
+      </SidebarContent>
+    </Sidebar>
+  )
+}
